b45b2f8c58da9b20e0d5f6a122f52e0ceff1fe74
spsv <32491878+spsv@users.noreply.github.com>
2020-09-13 22:16:19 -0600

    init

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Vazil App',
+      title: 'Vazil App1',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
